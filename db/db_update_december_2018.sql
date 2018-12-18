63c4bf74e544edca21db1b8bf5e93b55768a265f
nicolas flores <nicolasflores03@yahoo.com>
2018-12-18 15:00:16 +0800

    db update

---
+++ db/db_update_december_2018.sql
@@ -0,0 +1,169 @@
+USE [EAMDEV]
+GO
+  /*
+  ALTER TABLE [dbo].[R5_DEADLINE_MAINTENANCE]
+  ADD 
+	Q1 date null ,
+	Q2 date null ,
+	Q3 date null ,
+	Q4 date null ;
+
+	ALTER TABLE [dbo].[R5_BUDGET_MOVEMENT]
+	ADD 
+	target_quarter int null ;
+	*/
+ALTER VIEW [dbo].[R5_VIEW_BUDGET_MOVEMENT]
+AS
+SELECT     id, app_id, ORG_CODE, to_table, fr_table,
+                          (SELECT     TOP (1) MRC_DESC
+                            FROM          dbo.R5MRCS
+                            WHERE      (MRC_CODE COLLATE Latin1_General_CI_AS = dbo.R5_BUDGET_MOVEMENT.FR_MRC_CODE)) AS Source_Department, FR_MRC_CODE,
+                          (SELECT     TOP (1) MRC_DESC
+                            FROM          dbo.R5MRCS AS R5MRCS_1
+                            WHERE      (MRC_CODE COLLATE Latin1_General_CI_AS = dbo.R5_BUDGET_MOVEMENT.TO_MRC_CODE)) AS Destination_Department, TO_MRC_CODE, 
+                      CASE fr_table WHEN 'IB' THEN
+                          (SELECT     TOP (1) Description COLLATE Latin1_General_CI_AS
+                            FROM          dbo.R5_VIEW_ITEMBASE_LINES
+                            WHERE      (id = dbo.R5_BUDGET_MOVEMENT.fr_code)) ELSE
+                          (SELECT     TOP (1) description
+                            FROM          dbo.R5_VIEW_COSTBASE_LINES
+                            WHERE      (id = dbo.R5_BUDGET_MOVEMENT.fr_code)) END AS Source, fr_code, CASE to_table WHEN 'IB' THEN
+                          ((SELECT     TOP (1) Description COLLATE Latin1_General_CI_AS
+                              FROM         dbo.R5_VIEW_ITEMBASE_LINES AS R5_VIEW_ITEMBASE_LINES_1
+                              WHERE     (id = dbo.R5_BUDGET_MOVEMENT.to_code))) ELSE
+                          ((SELECT     TOP (1) description
+                              FROM         dbo.R5_VIEW_COSTBASE_LINES AS R5_VIEW_ITEMBASE_LINES_1
+                              WHERE     (id = dbo.R5_BUDGET_MOVEMENT.to_code))) END AS Destination, to_code, amount, to_available_amount, fr_available_amount, type, year_budget,  
+							  
+							  (CASE
+									WHEN
+									dbo.R5_BUDGET_MOVEMENT.target_quarter = 1
+										THEN
+										'Q1'
+									WHEN
+									dbo.R5_BUDGET_MOVEMENT.target_quarter = 2
+										THEN
+										'Q2'
+									WHEN
+									dbo.R5_BUDGET_MOVEMENT.target_quarter = 3
+										THEN
+										'Q3'
+									WHEN
+										dbo.R5_BUDGET_MOVEMENT.target_quarter = 4
+										  THEN
+											'Q4'
+									ELSE  
+										''
+								End ) as target_quarter, target_quarter as target_quarter_value,
+                      fr_cost_center, cost_center, status, reason, updatedAt, remarks
+					FROM  dbo.R5_BUDGET_MOVEMENT;
+				
+
+
+
+
+
+/****** Object:  Table [dbo].[R5_REF_ITEMBASE_BUDGET_QUARTERLY]  ******/
+
+ /*
+CREATE TABLE [dbo].[R5_REF_ITEMBASE_BUDGET_QUARTERLY](
+	[id] [int] NOT NULL,
+	[q1_total_cost] [numeric](24, 6) NULL,
+	[q1_adjustments] [numeric](24, 6) NULL,
+	[q1_available] [numeric](24, 6) NULL,
+	[q1_reserved] [numeric](24, 6) NULL,
+	[q1_allocated] [numeric](24, 6) NULL,
+	[q1_paid] [numeric](24, 6) NULL,
+
+	[q2_total_cost] [numeric](24, 6) NULL,
+	[q2_adjustments] [numeric](24, 6) NULL,
+	[q2_available] [numeric](24, 6) NULL,
+	[q2_reserved] [numeric](24, 6) NULL,
+	[q2_allocated] [numeric](24, 6) NULL,
+	[q2_paid] [numeric](24, 6) NULL,
+
+	[q3_total_cost] [numeric](24, 6) NULL,
+	[q3_adjustments] [numeric](24, 6) NULL,
+	[q3_available] [numeric](24, 6) NULL,
+	[q3_reserved] [numeric](24, 6) NULL,
+	[q3_allocated] [numeric](24, 6) NULL,
+	[q3_paid] [numeric](24, 6) NULL,
+	
+	[q4_total_cost] [numeric](24, 6) NULL,
+	[q4_adjustments] [numeric](24, 6) NULL,
+	[q4_available] [numeric](24, 6) NULL,
+	[q4_reserved] [numeric](24, 6) NULL,
+	[q4_allocated] [numeric](24, 6) NULL,
+	[q4_paid] [numeric](24, 6) NULL,
+	
+	[updatedAt] [datetime] NULL,
+	[updatedBy] [nchar](30) NULL,
+	[createdAt] [datetime] NULL,
+	[createdBy] [nchar](30) NULL
+) ON [PRIMARY];
+
+CREATE TABLE [dbo].[R5_REF_COSTBASE_BUDGET_QUARTERLY](
+	[id] [int] NOT NULL,
+	[q1_total_cost] [numeric](24, 6) NULL,
+	[q1_adjustments] [numeric](24, 6) NULL,
+	[q1_available] [numeric](24, 6) NULL,
+	[q1_reserved] [numeric](24, 6) NULL,
+	[q1_allocated] [numeric](24, 6) NULL,
+	[q1_paid] [numeric](24, 6) NULL,
+
+	[q2_total_cost] [numeric](24, 6) NULL,
+	[q2_adjustments] [numeric](24, 6) NULL,
+	[q2_available] [numeric](24, 6) NULL,
+	[q2_reserved] [numeric](24, 6) NULL,
+	[q2_allocated] [numeric](24, 6) NULL,
+	[q2_paid] [numeric](24, 6) NULL,
+
+	[q3_total_cost] [numeric](24, 6) NULL,
+	[q3_adjustments] [numeric](24, 6) NULL,
+	[q3_available] [numeric](24, 6) NULL,
+	[q3_reserved] [numeric](24, 6) NULL,
+	[q3_allocated] [numeric](24, 6) NULL,
+	[q3_paid] [numeric](24, 6) NULL,
+	
+	[q4_total_cost] [numeric](24, 6) NULL,
+	[q4_adjustments] [numeric](24, 6) NULL,
+	[q4_available] [numeric](24, 6) NULL,
+	[q4_reserved] [numeric](24, 6) NULL,
+	[q4_allocated] [numeric](24, 6) NULL,
+	[q4_paid] [numeric](24, 6) NULL,
+	
+	[updatedAt] [datetime] NULL,
+	[updatedBy] [nchar](30) NULL,
+	[createdAt] [datetime] NULL,
+	[createdBy] [nchar](30) NULL
+) ON [PRIMARY];
+
+
+ALTER VIEW [dbo].[R5_BUDGET_REALLOCATION_LOOKUP]
+AS
+SELECT     dbo.R5_DPP_VERSION.ORG_CODE, dbo.R5_DPP_VERSION.MRC_CODE, dbo.R5_DPP_VERSION.year_budget, dbo.R5_DPP_VERSION.version, 
+                      dbo.R5_DPP_VERSION.reference_no, dbo.R5_EAM_DPP_ITEMBASE_BRIDGE.rowid, dbo.R5_EAM_DPP_ITEMBASE_LINES.code, dbo.R5PARTS.PAR_DESC, 
+                      dbo.R5_DPP_VERSION.status, dbo.R5_EAM_DPP_ITEMBASE_LINES.available, dbo.R5_DPP_VERSION.cost_center,
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q1_available, 
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q2_available,
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q3_available,
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q4_available
+
+FROM         dbo.R5_EAM_DPP_ITEMBASE_BRIDGE INNER JOIN
+                      dbo.R5_DPP_VERSION ON dbo.R5_EAM_DPP_ITEMBASE_BRIDGE.reference_no = dbo.R5_DPP_VERSION.reference_no AND 
+                      dbo.R5_EAM_DPP_ITEMBASE_BRIDGE.version = dbo.R5_DPP_VERSION.version INNER JOIN
+                      dbo.R5_EAM_DPP_ITEMBASE_LINES ON dbo.R5_EAM_DPP_ITEMBASE_BRIDGE.rowid = dbo.R5_EAM_DPP_ITEMBASE_LINES.id INNER JOIN
+                      dbo.R5PARTS ON dbo.R5_EAM_DPP_ITEMBASE_LINES.code COLLATE Latin1_General_BIN = dbo.R5PARTS.PAR_CODE INNER JOIN
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY ON dbo.R5_EAM_DPP_ITEMBASE_LINES.record_id = dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.id
+
+GROUP BY dbo.R5_DPP_VERSION.ORG_CODE, dbo.R5_DPP_VERSION.MRC_CODE, dbo.R5_DPP_VERSION.year_budget, dbo.R5_DPP_VERSION.version, 
+                      dbo.R5_DPP_VERSION.reference_no, dbo.R5_EAM_DPP_ITEMBASE_BRIDGE.rowid, dbo.R5_EAM_DPP_ITEMBASE_LINES.code, dbo.R5PARTS.PAR_DESC, 
+                      dbo.R5_DPP_VERSION.status, dbo.R5_EAM_DPP_ITEMBASE_LINES.available, dbo.R5_DPP_VERSION.cost_center, 
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q1_available, 
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q2_available,
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q3_available,
+					  dbo.R5_REF_ITEMBASE_BUDGET_QUARTERLY.q4_available
+*/
+GO
+
+
